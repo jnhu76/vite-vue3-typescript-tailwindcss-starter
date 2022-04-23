af2d397686b5a4a276e2045d94af8a3dccf524e6
fred1653 <fred1653@qq.com>
2022-04-23 23:18:11 +0800

    add: add router

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<template>
+  <div class="container items-center mx-auto px-4">
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </div>
+</template>
+
+<style></style>
